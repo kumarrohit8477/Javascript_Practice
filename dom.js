7b8e31ecd1ccfe0d2aa1a17d753dedc28150d584
kumarrohit8477 <kumarrohitt0976@gmail.com>
2025-08-10 00:28:15 +0530

    add a new file

---
+++ dom.js
@@ -0,0 +1,26 @@
+/**document.querySelector('h1').textContent = 'Hello India';
+document.querySelector('h1').style.color = 'blue';
+document.querySelector('h1').style.backgroundColor = 'yellow';
+
+
+
+let cont = document.body.childNodes[3].firstElementChild;
+cont.style.backgroundColor = 'orange';
+
+let cont1 = document.body.childNodes[3].childNodes[5];
+cont1.style.backgroundColor = 'green';
+
+document.body.childNodes[3].lastChild.parentNode;
+document.body.childNodes[3].lastChild.parentNode;
+/**<div class=​"container">​…​</div>​<div class=​"box" style=​"background-color:​ orange;​">​box1 ​</div>​<div class=​"box">​box2 ​</div>​<div class=​"box" style=​"background-color:​ green;​">​box3 ​</div>​<div class=​"box">​box4 ​</div>​<div class=​"box">​box5 ​</div>​</div>​
+document.body.childNodes[3].childElementCount
+5
+document.body.childNodes[3].children
+HTMLCollection(5) [div.box, div.box, div.box, div.box, div.box]
+document.body.childNodes[3].element
+undefined
+document.body.childNodes[3]
+<div class=​"container">​…​</div>​
+document.body.childNodes.children**/
+
+console.log("hello world");
